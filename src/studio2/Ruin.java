2153dc27f52d775681c21b24fd9e171d3e47943e
eliseokeefe <112436436+eliseokeefe@users.noreply.github.com>
2023-02-01 18:56:25 -0600

    studio 2 - Ruin

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -10,22 +10,24 @@ public class Ruin {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		System.out.println("Start Amount: ");
-		int startAmount = in.nextInt();
+		double startAmount = in.nextDouble();
 		System.out.println("win chance: ");
 		double winChance = in.nextDouble();
 		System.out.println("win limit: ");
-		int winLimit = in.nextInt();
+		double winLimit = in.nextDouble();
 		System.out.println("total simulation time: ");
 		int simTime = in.nextInt();
 		int round = 0;
 		int day = 0;
 		double expectedRuin = 0;
 		boolean outcome = false;
+		double wins = 0; 
+		double losses = 0; 
 		for (day = 0; day < simTime; day++) {
-			int money = startAmount;
+			double money = startAmount;
 			while (money > 0 && money < winLimit) {
 				round++;
-				if (Math.random() < winChance) {
+				if (Math.random() <= winChance) {
 					money++;
 				} else {
 					money--;
@@ -35,17 +37,25 @@ public class Ruin {
 			if (money == 0) {
 				System.out.println("you lose");
 				outcome = true;
+				losses++; 
 			} else {
 				System.out.println("you win");
 				outcome = false;
+				wins++; 
 			}
 		} 
 		if (winChance == 0.5) {
-			expectedRuin = 1 - (startAmount/winLimit);
+			double math = startAmount/winLimit;
+			expectedRuin = (1 - math);
 		} else {
-			double a = (1-winChance)/winChance;
+			double a = ((1-winChance)/winChance);
 			expectedRuin = (Math.pow(a, startAmount) - Math.pow(a, winLimit))/(1-Math.pow(a, winLimit));
 		}
-		System.out.println(expectedRuin);
+		System.out.println("Losses: " + losses + "/" + (losses + wins)); 
+		System.out.println("Expected Ruin Rate: " + expectedRuin);
+		
+		double actualRuinRate = losses/(wins+losses); 
+		System.out.println("Actual Ruin Rate: " + actualRuinRate); 
 	}
+	
 }
